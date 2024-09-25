f8c5b1a3b4f8657a438bac892c5492d790ca685d
ethanhanky <efhankel@icloud.com>
2024-09-25 16:57:24 +0000

    Final

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# [Name]
-## [Assignment #]
-## [Submission date: MM/DD/YY]
+# [Ethan Hankel]
+## [Lab #3]
+## [Submission date: 09/24/24]
 ## Worked with/sources 
 * example
 * example
--- cosc1010-lab03.py
+++ cosc1010-lab03.py
@@ -1,8 +1,8 @@
-# Your Name Here
+# Ethan Hankel
 # UWYO COSC 1010
-# Submission Date
+# Submission Date: 09/24/24
 # Lab 03 
-# Lab Section: 
+# Lab Section: 14
 # Sources, people worked with, help given to: 
 # your
 # comments
@@ -18,34 +18,40 @@ print("Part One-----------------------------------------------------------------
 #We are going to start with the basics. Declare a list  states that contains the elements: Wyoming, Colorado, Montana in that order 
 #Note this is the ONLY point where you need to declare the states list
 
-
+states = ["Wyoming", "Colorado", "Montana"]
 
 #print the entire list
-
+print(states)
 
 #now print the first element in the list
 
-
+print(states[0])
 #Print the last element using the syntax shown in class to access the final element (hint, think negatives)
 
-
+print(states[-1])
 #Using an F-string to access the first and second element print the string "COLORADO is south of WYOMING", matching the casing provided
 
-
+print(f"{states[1].upper()} is south of {states[0].upper()}")
 
 
 print("Part Two------------------------------------------------------------------------")
 #Append the following states to your list: Washington, Oregon, California and print your list
 
-
+states.append("Washington")
+states.append("Oregon")
+states.append("California")
+print(states)
 #Again using the specific syntax mentioned in class overwrite the second to last element to be Maine, printing the list 
-
+states[5] = "Maine"
+print(states)
 
 #Insert the state Texas to be the third element in the list, again printing your list
-
+states.insert(2,"Texas") 
+print(states)
 
 #Using the `del` statement remove the fourth item from the list, print your list 
-
+del(states[3])
+print(states)
 
 #Remove Texas using its value, print the list
 
